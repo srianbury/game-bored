381e103779f1d0741eaf28ec6b5645966368cc29
srianbury <bsunbury29@gmail.com>
2022-06-03 16:31:25 -0700

    No Stylesheets In Head Component

--- components/WrapRoot/WrapRoot.tsx
+++ components/WrapRoot/WrapRoot.tsx
@@ -10,10 +10,6 @@ const WrapRoot: FC<AppProps> = ({ Component, pageProps }) => {
     <>
       <CssBaseline />
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
         <meta name="description" content="Game Bored" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
---
+++ pages/_document.tsx
@@ -0,0 +1,3 @@
+import { Document } from "../components";
+
+export default Document;
